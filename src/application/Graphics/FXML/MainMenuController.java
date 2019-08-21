f0bfb642e8c186d70281abc9e9b38b5096878743
Davide Caffagni <davidecaffagni98@gmail.com>
2019-08-21 11:01:10 +0200

    acab

--- src/application/Graphics/FXML/MainMenuController.java
+++ src/application/Graphics/FXML/MainMenuController.java
@@ -1,9 +1,23 @@
 package application.Graphics.FXML;
 
+import application.Graphics.item.panes.MainPane;
+import application.Graphics.item.scenes.MainScene;
+import application.Graphics.item.stages.MainStage;
+import application.Main;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainMenuController {
 
@@ -11,11 +25,24 @@ public class MainMenuController {
     BorderPane MainMenuBorderPanel;
 
     @FXML
-    ImageView Titolo;
+    ImageView Title;
 
     @FXML
     VBox MainBox;
 
+    @FXML
+    Button playButton;
+
+    @FXML
+    Button scoreButton;
+
+    @FXML
+    Button songsButton;
+
+    @FXML
+    Button exitButton;
+
+    private List<Button> buttons;
 
     public MainMenuController(){
     }
@@ -23,12 +50,44 @@ public class MainMenuController {
 
     public void initialize() {
 
+        buttons = new ArrayList<Button>();
+        buttons.add(playButton);
+        buttons.add(scoreButton);
+        buttons.add(songsButton);
+        buttons.add(exitButton);
 
-        System.out.println("sono stato qui");
         MainBox.prefHeightProperty().bind(MainMenuBorderPanel.heightProperty());
         MainBox.prefWidthProperty().bind(MainMenuBorderPanel.widthProperty());
-        Titolo.fitWidthProperty().bind(MainMenuBorderPanel.widthProperty());
-        Titolo.fitHeightProperty().bind(MainMenuBorderPanel.heightProperty());
+        //Title.fitWidthProperty().bind(MainMenuBorderPanel.widthProperty());
+        Title.fitHeightProperty().bind(MainMenuBorderPanel.heightProperty());
+        //playButton.prefWidthProperty().bind(MainBox.widthProperty());
+        //playButton.prefHeightProperty().bind(MainBox.heightProperty());
+
+        MainMenuBorderPanel.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                Title.setFitWidth(MainMenuBorderPanel.getWidth() * 0.3);
+                // 4 bottoni, 3 spacing  15 10 15 10 15 10 15
+                MainBox.setSpacing(MainBox.getHeight() * 0.1);
+                MainBox.setPadding(new Insets(MainBox.getHeight() * 0.05, MainBox.getWidth() * 0.05, MainBox.getHeight() * 0.05, MainBox.getWidth() * 0.05));
+                for (Button b: buttons) {
+
+                    b.setPrefWidth(MainBox.getWidth() * 0.2);
+                    b.setPrefHeight(MainBox.getHeight() * 0.15);
+                }
+
+            }
+        });
+    }
+
+
+    @FXML
+    public void playOnClick () {
+
+        System.out.println("Clikkato!!");
+        //Parent root = FXMLLoader.load(getClass().getResource("mainGUI.fxml"));
+        MainStage mainStage = (MainStage) MainMenuBorderPanel.getScene().getWindow();
+        mainStage.setScene(new MainScene(new MainPane(), mainStage.getWidth(), mainStage.getHeight()));
 
     }
 
--- src/application/Graphics/item/panes/MenuPane.java
+++ src/application/Graphics/item/panes/MenuPane.java
@@ -8,17 +8,12 @@ import javafx.scene.paint.Color;
 public class MenuPane extends TilePane {
     public MenuPane() {
 
-     /*   menu.setLayoutX(mainPane.getWidth() * 0.4);
+     /* menu.setLayoutX(mainPane.getWidth() * 0.4);
         menu.setLayoutY(mainPane.getHeight() * 0.4);
         menu.setPrefWidth(mainPane.getWidth() * 0.2);
         menu.setPrefHeight(mainPane.getHeight() * 0.5);
         menu.setPrefTileWidth(mainPane.getWidth() * 0.2);
         menu.setPrefTileHeight(mainPane.getHeight() * 0.5 / menuButtons.size());*/
         setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
-
-
-
-
-
     }
 }
